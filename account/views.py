36c3801f66a17d0db18f3b703abbeb2632fb01d8
Eyimofe Ogunbiyi <ogunbiyioladapo33@gmail.com>
2021-04-19 16:36:47 +0100

    create user styling

--- account/views.py
+++ account/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
+from .forms import AuthorInfo
 
 
 # Create your views here.
 
-def create_user():
-    pass
+def create_user(request):
+    if request.method == 'POST':
+        form = AuthorInfo(request.POST)
+        if form.is_valid():
+            form.save()
+            context = {
+                " successMessage": "user created successfully"
+            }
+            return render(request, 'success.html', context)
+    else:
+        form = AuthorInfo()
+        context = {
+            "form": form
+        }
+        return render(request, 'users/create_user.html', context)
